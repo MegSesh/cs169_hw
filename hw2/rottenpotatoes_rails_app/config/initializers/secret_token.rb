846b01d0e84a13b430901c2689c81e41f835b396
saasbook <saasbook@saasbook.(none)>
2015-02-14 17:01:17 -0400

    pushing hw2 onto github

---
+++ hw2/rottenpotatoes_rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottenpotatoes::Application.config.secret_token = '595c83d1826b87f73ee71f1fccf16d38768e79055c667558d412dc5b3f9482cc5b336f88b9afa12e2e96981f81659363d8cd32f67e240eb399b1ef1b418d6329'
