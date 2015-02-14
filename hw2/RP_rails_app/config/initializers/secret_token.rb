2b1592ec297396ee94f8720c14a30011ca1acc7a
saasbook <saasbook@saasbook.(none)>
2015-02-14 19:20:16 -0400

    creating new hw2 rails app

---
+++ hw2/RP_rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RPRailsApp::Application.config.secret_token = '902c367d9f203cc9eea1eeb60635167fefcbf97f28d9a257eb26e4403985848ef3f3f7dac2cb2cc27f153ffb90e0c2158e05df7bad7700ad9dd5469ec97c5daf'
