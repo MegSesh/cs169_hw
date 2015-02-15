8d31dc0c7b08ed483b2864295862cb6a1d1d3e5d
saasbook <saasbook@saasbook.(none)>
2015-02-15 14:10:11 -0400

    created new RP3 rails app. lets see if it works this time

---
+++ hw2/RP3/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RP3::Application.config.secret_token = 'bc65c31d5e169069434f3cd042c3cb828378c18021c8e6ddebd578e9c85739d9aeed17c364eb3fe33540c3fd0574fe50321c45325abd5581ab3319f0338b41e6'
